aa79369293fcaad0fe5cf865db4acac2cdf11002
lucas <lucas@LAPTOP-PAQTMCQ7>
2022-07-06 13:37:44 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto5_Prova_Djalma/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto5_Prova_Djalma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1, num;
+
+            while(i <= 100)
+            {
+                i++;
+                num = i % 2;
+
+                if (num == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
